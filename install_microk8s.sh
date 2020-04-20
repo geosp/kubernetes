6c30f06c640c2ed140e074c9d033241e57b1e72c
Geovanny Fajardo <gfajardo@govspend.com>
2020-04-20 15:26:09 -0400

    Exposing registry.

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -2,6 +2,8 @@ sudo snap install microk8s --classic
 sudo snap alias microk8s.kubectl kubectl
 microk8s enable dashboard dns ingress istio metallb metrics-server registry storage
 microk8s kubectl expose deployment kubernetes-dashboard --type=LoadBalancer --name=kubernetes-dashboard-external --namespace=kube-system
+microk8s kubectl expose deployment registry --type=LoadBalancer --name=registry-external --namespace=container-registry
 microk8s kubectl get services --namespace=kube-system
+microk8s kubectl get services --namespace=container-registry
 token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
 microk8s kubectl -n kube-system describe secret $token
